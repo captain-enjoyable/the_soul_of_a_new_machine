23df0b1905ad777fd859e32577941be615dcd92a
Matt Eddy <matteddy1@gmail.com>
2022-01-23 12:58:08 -0800

    Add some profile stuff

---
+++ profile_stuff.sh
@@ -0,0 +1,11 @@
+function gb() {
+  git branch --sort=committerdate | tail -r | awk -F ' +' '! /\(no branch\)/ {print $2}' | fzf | xargs git checkout
+}
+
+function cf() {
+  git diff --name-only  $(git rev-parse --abbrev-ref HEAD)  $(git merge-base $(git rev-parse --abbrev-ref HEAD) master) | fzf | xargs code
+}
+
+function projects() {
+  cd ~/Projects/$(find ~/Documents/projects -type d -maxdepth 2 -depth 1 | sed 's/\/Users\/mattheweddy\/Projects\///' | fzf -1 -q "$1")
+}
